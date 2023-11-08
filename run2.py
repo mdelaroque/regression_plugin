c6f9dadaa3dac0de274b691283515b44f8d60cd1
mdelaroque <mdelaroque@steelperlot.com>
2023-11-08 16:59:34 -0500

    Add the python version (hard to tell if there is some improvements on such a small df)

--- run2.py
+++ run2.py
@@ -1,7 +1,17 @@
 import polars as pl
+from functools import partial
 from expression_lib import Language, Distance, Regr
 from datetime import date
 import numpy as np
+
+
+def regress_multi(s: pl.Series, yvar: str, xvars: list[str]) -> pl.Series:
+    df = s.struct.unnest()
+    yvar = df[[yvar]].to_numpy()
+    xvars = np.hstack([df[xvars].to_numpy(), np.ones((len(s), 1))])
+    lsqres = np.linalg.lstsq(xvars, yvar)
+    results = lsqres[0][:, 0]
+    return pl.Series(results)
 
 df = pl.DataFrame(
     {
@@ -60,4 +70,11 @@ out = df.lazy().with_columns(pl.lit(1.).alias("intercept")).groupby("names").agg
         # pl.lit(1.).alias("intercept"),  ## Somehow adding one here kill the core, looking at what is passed through to rust, it's only 1 float
         ),
 ).collect()
+print("\n",out)
+
+out = df.lazy().with_columns(pl.lit(1.).alias("intercept")).groupby("names").agg(
+                pl.struct(["floats", "floats2",(pl.col("floats")**2).alias("to_the_power2")]).apply(
+                    partial(regress_multi, xvars=["floats2", "to_the_power2"], yvar="floats")
+                )
+            ).collect()
 print("\n",out)
